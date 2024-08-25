f20f2acfde261ca5745121cf08189806440cc114
Paul Fioravanti <paul.fioravanti@gmail.com>
2024-08-25 21:24:18 +1000

    Remove Python SDK from test dependencies

--- setup.py
+++ setup.py
@@ -2,8 +2,4 @@
 
 from setuptools import setup
 
-setup(
-    extras_require = {
-        "test": ["onepassword @ git+ssh://git@github.com/1Password/onepassword-sdk-python.git@v0.1.1"]
-    }
-)
+setup()
