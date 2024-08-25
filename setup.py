e320c08c51bf8b10daca19796315d56953354a90
Paul Fioravanti <paul.fioravanti@gmail.com>
2024-08-25 19:48:25 +1000

    Enable onepassword library in test suite

--- setup.py
+++ setup.py
@@ -2,4 +2,8 @@
 
 from setuptools import setup
 
-setup()
+setup(
+    extras_require = {
+        "test": ["onepassword @ git+ssh://git@github.com/1Password/onepassword-sdk-python.git@v0.1.1"]
+    }
+)
